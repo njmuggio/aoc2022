8c750e39c0219a7aa7bfba582117946d9178af4a
Nick Muggio <nick@mugg.io>
2022-12-17 00:00:00 -0500

    Day 16... but not really. This one stumped me

---
+++ 16/part1.py
@@ -0,0 +1,73 @@
+# file = "demo"
+file = "input"
+
+class Valve:
+  def __init__(self, name: str, rate: int, dests: list[str]) -> None:
+    self.name = name
+    self.rate = rate
+    self.dests = {}
+    for name in dests:
+      self.dests[name] = None
+    self.maxVal = {(0, ""): 0}
+
+  def update(self, valves):
+    for key in self.dests.keys():
+      self.dests[key] = valves[key]
+
+
+  def findMaxValue(self, remaining: int, openValves: str) -> int:
+    if (remaining, openValves) in self.maxVal:
+      return self.maxVal[(remaining, openValves)]
+
+    if remaining == 0:
+      return 0
+
+    maxVal = 0
+
+    if self.rate > 0 and self.name not in openValves and remaining > 2:
+      releaseValue = self.rate * (remaining - 1)
+      withOpen = openValves + " " + self.name
+
+      for dest in self.dests.values():
+        value = releaseValue + dest.findMaxValue(remaining - 2, withOpen)
+        if value > maxVal:
+          maxVal = value
+          # print(f"{self.name} open -> {dest.name}")
+
+
+    for dest in self.dests.values():
+      value = dest.findMaxValue(remaining - 1, openValves)
+      if value > maxVal:
+        maxVal = value
+        # print(f"{self.name}      -> {dest.name}")
+
+    self.maxVal[(remaining, openValves)] = maxVal
+    return maxVal
+
+
+with open(file, mode="rt") as file:
+
+  valves = {}
+
+  for line in (line.strip() for line in file):
+    words = line.split(" ")
+    name = words[1]
+    rate = int(words[4][5:-1])
+    dests = []
+    for i in range(9, len(words)):
+      if words[i][-1] == ",":
+        dests.append(words[i][:-1])
+      else:
+        dests.append(words[i])
+
+    valve = Valve(name, rate, dests)
+    valves[name] = valve
+
+for valve in valves.values():
+  valve.update(valves)
+
+for i in range(1, 31):
+  for valve in valves.values():
+    valve.findMaxValue(i, "")
+
+print(valves["AA"].findMaxValue(30, ""))
